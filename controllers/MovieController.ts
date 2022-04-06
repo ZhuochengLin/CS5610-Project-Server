9a1b063076c386b7cda2777e7631d27df3efad9e
Zhuocheng Lin <zhuochenglin@zhuochengs-mbp.myfiosgateway.com>
2022-04-06 19:43:52 -0400

    WP-67 Add search endpoint

--- controllers/MovieController.ts
+++ controllers/MovieController.ts
@@ -1,8 +1,10 @@
 import {Express, NextFunction, Request, Response} from "express";
 import {pipeline} from "stream";
 import got from "got";
+import {InvalidInputError} from "../errors/CustomErrors";
 
-const TMDB_BASE_URL = "https://api.themoviedb.org/3/movie"
+const TMDB_BASE_URL = "https://api.themoviedb.org/3";
+const TMDB_MOVIE_BASE_URL = TMDB_BASE_URL + "/movie";
 
 export default class MovieController {
 
@@ -16,6 +18,7 @@ export default class MovieController {
             app.get("/api/movies/nowplaying/:page", MovieController.movieController.findNowPlayingMovies);
             app.get("/api/movies/toprated/:page", MovieController.movieController.findTopRatedMovies);
             app.get("/api/movies/upcoming/:page", MovieController.movieController.findUpcomingMovies);
+            app.get("/api/movies", MovieController.movieController.searchMovie);
         }
         return MovieController.movieController;
     }
@@ -26,7 +29,7 @@ export default class MovieController {
     findPopularMovies = async (req: Request, res: Response, next: NextFunction) => {
         let page = req.params.page;
         const dataStream = got.stream.get(
-            `${TMDB_BASE_URL}/popular`,
+            `${TMDB_MOVIE_BASE_URL}/popular`,
             {searchParams: {api_key: process.env.TMDB_API_KEY, page: page}}
         );
         pipeline(dataStream, res, (err) => {
@@ -36,7 +39,7 @@ export default class MovieController {
 
     findMovieDetails = async (req: Request, res: Response, next: NextFunction) => {
         const dataStream = got.stream.get(
-            `${TMDB_BASE_URL}/${req.params.mid}`,
+            `${TMDB_MOVIE_BASE_URL}/${req.params.mid}`,
             {searchParams: {api_key: process.env.TMDB_API_KEY}}
         );
         pipeline(dataStream, res, (err) => {
@@ -47,7 +50,7 @@ export default class MovieController {
     findNowPlayingMovies = async (req: Request, res: Response, next: NextFunction) => {
         let page = req.params.page;
         const dataStream = got.stream.get(
-            `${TMDB_BASE_URL}/now_playing`,
+            `${TMDB_MOVIE_BASE_URL}/now_playing`,
             {searchParams: {api_key: process.env.TMDB_API_KEY, page: page}}
         );
         pipeline(dataStream, res, (err) => {
@@ -58,7 +61,7 @@ export default class MovieController {
     findTopRatedMovies = async (req: Request, res: Response, next: NextFunction) => {
         let page = req.params.page;
         const dataStream = got.stream.get(
-            `${TMDB_BASE_URL}/top_rated`,
+            `${TMDB_MOVIE_BASE_URL}/top_rated`,
             {searchParams: {api_key: process.env.TMDB_API_KEY, page: page}}
         );
         pipeline(dataStream, res, (err) => {
@@ -67,10 +70,27 @@ export default class MovieController {
     }
 
     findUpcomingMovies = async (req: Request, res: Response, next: NextFunction) => {
-        let page = req.params.page;
+        const page = req.params.page;
         const dataStream = got.stream.get(
-            `${TMDB_BASE_URL}/upcoming`,
+            `${TMDB_MOVIE_BASE_URL}/upcoming`,
             {searchParams: {api_key: process.env.TMDB_API_KEY, page: page}}
+        );
+        pipeline(dataStream, res, (err) => {
+            if (err) next(err)
+        });
+    }
+
+    searchMovie = async (req: Request, res: Response, next: NextFunction) => {
+        const movie = req.query.movie;
+        const page = req.query.page;
+        if (!movie || !page) {
+            next(new InvalidInputError("Please provide {movie} and {page} parameters."));
+            return
+        }
+        const dataStream = got.stream.get(
+            `${TMDB_BASE_URL}/search/movie`,
+            // @ts-ignore
+            {searchParams: {api_key: process.env.TMDB_API_KEY, query: movie, page: page}}
         );
         pipeline(dataStream, res, (err) => {
             if (err) next(err)
